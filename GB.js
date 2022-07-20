74a146a14d8dc6f6f46caf27376efee64d068bb5
Keron Lamb <keron.lamb@dfcbelize.org>
2022-07-20 14:03:45 -0600

    update

--- GB.js
+++ GB.js
@@ -1,3 +1,9 @@
+const _ = require('lodash');
+
+const array = [1,2,3,4,56,7,8]
+
+console.log('answer',_.without(array, 56));
+
 const css = document.querySelector("h3");
 const color = document.querySelector(".color");
 const color1 = document.querySelector(".color1");
